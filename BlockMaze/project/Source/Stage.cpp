9ac891ec0a5cd6e1cfe73339e489c2c0ff0327d8
SueKazuma <sdt2495gp0009@edu.sca.ac.jp>
2024-10-10 11:48:38 +0900

    10/10、マップチップ表示まで

--- BlockMaze/project/Source/Stage.cpp
+++ BlockMaze/project/Source/Stage.cpp
@@ -1,5 +1,25 @@
 #include "Stage.h"
 
+//const:書き換えられない定数
+//直接数字を書くと人格否定される
+//数字ではなく文字で書く
+const int WIDTH = 12;
+const int HIGHT = 8;
+const int CHIP_SIZE = 40;
+
+int map[HIGHT][WIDTH] = 
+{
+	{2,1,1,1,1,1,1,1,1,1,1,2},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,1,1,1,1,1,1,1,1,0,1},
+	{1,0,1,0,0,0,0,0,0,1,0,1},
+	{1,0,1,0,0,0,0,0,0,1,0,1},
+	{1,0,1,1,1,1,1,1,1,1,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{2,1,1,1,1,1,1,1,1,1,1,2},
+};
+
+
 Stage::Stage()
 {
 	hImage = LoadGraph("data/image/parts.png");
@@ -7,9 +27,28 @@ Stage::Stage()
 
 Stage::~Stage()
 {
+	
 }
 
 void Stage::Draw()
 {
-	DrawRectGraph(100, 100, 0, 40, 40, 40, hImage, TRUE);
+	//x,y,切り抜き,切り抜き,40,40
+	for (int j = 0; j < HIGHT; j++)//縦「j」
+	{
+		int y = j * 40 + 100;
+		for (int i = 0; i < WIDTH; i++)//幅「i」
+		{
+			int x = i * 40 + 100;
+			if (map[j][i] == 1)//レンガ
+			{
+				DrawRectGraph(x, y, CHIP_SIZE*0, CHIP_SIZE*1, 40, 40, hImage, TRUE);
+			}
+			else if (map[j][i] == 2)//コイン
+			{
+			DrawRectGraph(x, y, CHIP_SIZE*3, CHIP_SIZE*0, 40, 40, hImage, TRUE);
+			}
+		}
+	}
+
+	
 }
