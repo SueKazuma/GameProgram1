da2d4a6bc832361d62daea3a2e75b1f17db26f14
SueKazuma <sdt2495gp0009@edu.sca.ac.jp>
2025-10-02 17:00:31 +0900

    871上書き

--- SwordBout/project/Source/PadInput.cpp
+++ SwordBout/project/Source/PadInput.cpp
@@ -1,195 +1,180 @@
-﻿#include "PadInput.h"
+#include "PadInput.h"
 #include <assert.h>
+#define USE_MOUSE_CAMERA // �}�E�X�ŉE�X�e�B�b�N�̕ς��
+#define USE_KEYBOARD_MOVE // �L�[�{�[�h�ňړ�
 
-PadInput::PadInput(int id) 
+static const int MAX = 32767;
+static const int LIMIT = MAX / 5;
+
+PadInput::PadInput(int id)
 {
-
-
-	padId = id;
-	DontDestroyOnSceneChange();
+    pad_id = id;
+    DontDestroyOnSceneChange();
+#ifdef USE_MOUSE_CAMERA // �}�E�X�ŉE�X�e�B�b�N�̕ς��
+    GetMousePoint(&prevMouseX, &prevMouseY);
+#endif
 }
 
-PadInput::~PadInput() 
+PadInput::~PadInput()
 {
 }
 
-void PadInput::Update()
-{
-	for (int i = 0; i < 16; i++)
-	{
-		prevButtons[i] = input.Buttons[i];
-	}
-	GetJoypadXInputState(padId, &input);
-}
-
-void PadInput::Draw()
-{
-	DrawFormatString(200, 50, GetColor(255, 255, 255), "LStick %d %d", input.ThumbLX, input.ThumbLY);
+float StickValue(int stick) {
+    if (-LIMIT < stick && stick < LIMIT)
+    {
+        return 0.0f;
+    } else if (stick > 0)
+    {
+        float ret = (float)(stick - LIMIT) / (MAX - LIMIT);
+        if (ret > 1.0f) {
+            ret = 1.0f;
+        }
+        return ret;
+    } else
+    {
+        float ret = (float)(stick + LIMIT) / (MAX - LIMIT);
+        if (ret < -1.0f) {
+            ret = -1.0f;
+        }
+        return ret;
+    }
 }
 
 float PadInput::LStickX()
 {
-	if (input.ThumbLX >= ZERO_RANGE) 
-	{
-		float ret = (float)(input.ThumbLX - ZERO_RANGE) / (MAX - ZERO_RANGE);
-		// +１を上回ってしまった場合(念のため)
-		if (ret > 1.0f) 
-		{
-			ret = 1.0f;
-		}
-		return ret;
-	}
-	else
-	{
-		if (input.ThumbLX <= -ZERO_RANGE) 
-		{
-			float ret = (float)(input.ThumbLX + ZERO_RANGE) / (MAX - ZERO_RANGE);
-			// -１を下回ってえてしまった場合(念のため)
-			if (ret < -1.0f)
-			{
-				ret = -1.0f;
-			}
-			return ret;
-		}
-	}
-	return 0.0f;
+    return StickValue(input.ThumbLX);
 }
 
 float PadInput::LStickY()
 {
-	if (input.ThumbLY >= ZERO_RANGE)
-	{
-		float ret = (float)(input.ThumbLY - ZERO_RANGE) / (MAX - ZERO_RANGE);
-		// +１を上回ってしまった場合(念のため)
-		if (ret > 1.0f)
-		{
-			ret = 1.0f;
-		}
-		return ret;
-	}
-	else
-	{
-		if (input.ThumbLY <= -ZERO_RANGE)
-		{
-			float ret = (float)(input.ThumbLY + ZERO_RANGE) / (MAX - ZERO_RANGE);
-			// -１を下回ってえてしまった場合(念のため)
-			if (ret < -1.0f)
-			{
-				ret = -1.0f;
-			}
-			return ret;
-		}
-	}
-	return 0.0f;
+    return StickValue(input.ThumbLY);
 }
 
 float PadInput::RStickX()
 {
-	if (input.ThumbRX >= ZERO_RANGE)
-	{
-		float ret = (float)(input.ThumbRX - ZERO_RANGE) / (MAX - ZERO_RANGE);
-		// +１を上回ってしまった場合(念のため)
-		if (ret > 1.0f)
-		{
-			ret = 1.0f;
-		}
-		return ret;
-	}
-	else
-	{
-		if (input.ThumbRX <= -ZERO_RANGE)
-		{
-			float ret = (float)(input.ThumbRX + ZERO_RANGE) / (MAX - ZERO_RANGE);
-			// -１を下回ってえてしまった場合(念のため)
-			if (ret < -1.0f)
-			{
-				ret = -1.0f;
-			}
-			return ret;
-		}
-	}
+    return StickValue(input.ThumbRX);
 }
 
 float PadInput::RStickY()
 {
-	if (input.ThumbRY >= ZERO_RANGE)
-	{
-		float ret = (float)(input.ThumbRY - ZERO_RANGE) / (MAX - ZERO_RANGE);
-		// +１を上回ってしまった場合(念のため)
-		if (ret > 1.0f)
-		{
-			ret = 1.0f;
-		}
-		return ret;
-	}
-	else
-	{
-		if (input.ThumbRY <= -ZERO_RANGE)
-		{
-			float ret = (float)(input.ThumbRY + ZERO_RANGE) / (MAX - ZERO_RANGE);
-			// -１を下回ってえてしまった場合(念のため)
-			if (ret < -1.0f)
-			{
-				ret = -1.0f;
-			}
-			return ret;
-		}
-	}
-	return 0.0f;
+    return StickValue(input.ThumbRY);
+}
+
+VECTOR2 StickVec(int x, int y)
+{
+    float size = sqrtf((float)x*x + (float)y*y);
+    if (size < LIMIT)
+    {
+        return VECTOR2(0, 0);
+    }
+    else if (size > MAX)
+    {
+        return VECTOR2(x / size, y / size);
+    }
+    else
+    {
+        return VECTOR2(x / (float)MAX, y / (float)MAX);
+    }
 }
 
 VECTOR2 PadInput::LStickVec()
 {
-	VECTOR2 ret = VECTOR2(LStickX(), LStickY());
-	if (ret.Size() > 1.0f) 
-	{
-		ret = ret.Normalize();
-	}
-	return ret;
+    VECTOR2 ret = VECTOR2(LStickX(), LStickY());
+    if (CheckHitKey(KEY_INPUT_W)) {
+        ret += VECTOR2(0, 1);
+    }
+    if (CheckHitKey(KEY_INPUT_S)) {
+        ret += VECTOR2(0, -1);
+    }
+    if (CheckHitKey(KEY_INPUT_D)) {
+        ret += VECTOR2(1, 0);
+    }
+    if (CheckHitKey(KEY_INPUT_A)) {
+        ret += VECTOR2(-1, 0);
+    }
+    if (ret.Size() > 1.0f) {
+        ret = ret.Normalize();
+    }
+    return ret;
 }
 
 VECTOR2 PadInput::RStickVec()
 {
-	VECTOR2 ret = VECTOR2(RStickX(), RStickY());
-
-	// ←Key（動かん😿）
-	if (CheckHitKey(KEY_INPUT_W))
-	{
-		ret += VECTOR2(0, 1);
-	}
-	if (CheckHitKey(KEY_INPUT_S))
-	{
-		ret += VECTOR2(0, -1);
-	}
-	if (CheckHitKey(KEY_INPUT_D))
-	{
-		ret += VECTOR2(1, 0);
-	}
-	if (CheckHitKey(KEY_INPUT_A))
-	{
-		ret += VECTOR2(-1, 0);
-	}
-
-	if (ret.Size() > 1.0f)
-	{
-		ret = ret.Normalize();
-	}
-	return ret;
+    return StickVec(input.ThumbRX, input.ThumbRY);
 }
 
 bool PadInput::Press(int id)
 {
-	assert(id < 16); //?
-	return input.Buttons[id] > 0;
+    assert(id < 16);
+    return input.Buttons[id] > 0;
 }
 
 bool PadInput::OnPush(int id)
 {
-	assert(id < 16); //?
-	return input.Buttons[id] > 0 && prevButtons[id] == 0;
-
+    assert(id < 16);
+    return input.Buttons[id] > 0 && prevButtons[id] == 0;
 }
 
 void PadInput::Reset()
 {
+    GetMousePoint(&prevMouseX, &prevMouseY);
+    for (int i = 0; i < 16; i++)
+    {
+        prevButtons[i] = 1;
+    }
+}
+
+inline int clamp(int v, int minV, int maxV)
+{
+    if (v < minV)
+        return minV;
+    if (v > maxV)
+        return maxV;
+    return v;
+}
+
+void PadInput::Update()
+{
+
+    for (int i = 0; i < 16; i++)
+    {
+        prevButtons[i] = input.Buttons[i];
+    }
+    GetJoypadXInputState(pad_id, &input);
+
+
+
+#ifdef USE_KEYBOARD_MOVE
+    if (CheckHitKey(KEY_INPUT_W))
+    {
+        input.ThumbLY = -MAX;
+    }
+    if (CheckHitKey(KEY_INPUT_S))
+    {
+        input.ThumbLY = MAX;
+    }
+    if (CheckHitKey(KEY_INPUT_D))
+    {
+        input.ThumbLX = MAX;
+    }
+    if (CheckHitKey(KEY_INPUT_A))
+    {
+        input.ThumbLX = -MAX;
+    }
+#endif
+#ifdef USE_MOUSE_CAMERA
+    static const int SENSE = 40;
+    int mouseX, mouseY;
+    GetMousePoint(&mouseX, &mouseY);
+    int dragX = (mouseX - prevMouseX) * MAX / SENSE;
+    int dragY = (mouseY - prevMouseY) * MAX / SENSE;
+    input.ThumbRX = clamp(dragX + input.ThumbRX, -MAX, MAX);
+    input.ThumbRY = clamp(dragY + input.ThumbRY, -MAX, MAX);
+    prevMouseX = mouseX;
+    prevMouseY = mouseY;
+#endif
+    if (GetMouseInput() & MOUSE_INPUT_LEFT)
+    {
+        input.Buttons[XINPUT_BUTTON_X] = 1;
+    }
 }
